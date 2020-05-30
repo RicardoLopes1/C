8c9403113cd3b9f6170852a68260b43a337eb4f5
Ricardo Lopes <prslopes1@gmail.com>
2020-05-30 20:23:08 -0300

    Add 'i<=x/i' at primo function in 'for'.

--- NepsAcademy/Computational_MathCpp/Primos/Eh_primo.cpp
+++ NepsAcademy/Computational_MathCpp/Primos/Eh_primo.cpp
@@ -18,7 +18,7 @@ int main(){
 bool eh_primo(long long int x){
 	if(x == 1) return false;
 
-	for (int i = 2; i < x/i; i++){
+	for (int i = 2; i <= x/i; i++){
 		if (x % i == 0)
 			return false;
 	}
